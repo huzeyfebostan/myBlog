5680f326b171d4c88673390e4c40f3f39d83e684
huzeyfebostan <huzeyfebostan99@gmail.com>
2022-09-09 17:59:02 +0300

    hash-password

--- models/user.go
+++ models/user.go
@@ -1,9 +1,25 @@
 package models
 
+import (
+	"golang.org/x/crypto/bcrypt"
+	"gorm.io/gorm"
+)
+
 type User struct {
+	gorm.Model
 	Id        uint   `json:"id"`
 	FirstName string `json:"firstname"`
 	LastName  string `json:"lastname"`
 	Email     string `json:"email"`
-	Password  string `json:"password"`
+	Password  []byte `json:"password"`
+}
+
+func HashPassword(password string) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+	return string(bytes), err
+}
+
+func CheckPasswordHash(password, hash string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	return err == nil
 }
